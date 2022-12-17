cd33aaa1fb832a9c2689294f718bc9b6f708d8d3
Duy Phong Nguyen <89725748+dphonguyn-digiex@users.noreply.github.com>
2022-12-17 17:06:38 +0700

    update lib ver

--- client/src/index.js
+++ client/src/index.js
@@ -2,15 +2,15 @@ import React from "react";
 import ReactDOM from "react-dom";
 import { Provider } from "react-redux";
 import { createStore, applyMiddleware } from "redux";
-import { composeWithDevTools } from "redux-devtools-extension";
+import { composeWithDevTools } from "@redux-devtools/extension";
 import { BrowserRouter } from "react-router-dom";
 import createSagaMiddleware from "redux-saga";
 
 import { ContextProvider } from "./context/ThemeContext.js";
 import App from "./App.jsx";
 
-import mySaga from "~/store/sagas";
-import reducers from "~/store/reducers";
+import mySaga from "./store/sagas/index.js";
+import reducers from "./store/reducers/index.js";
 
 import "./index.css";
 
